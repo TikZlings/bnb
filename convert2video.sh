41be17dac679bd98e01d4fe38a9a07f9932ba19e
samcarter <sam.carter8@aol.com>
2019-01-29 12:53:16 +0100

    added video conversion script

---
+++ convert2video.sh
@@ -0,0 +1,19 @@
+# clean up old video
+rm cheering.mp4
+
+# convert to png images
+convert -density 160 cheering.pdf cheering.png
+
+# convert to video
+#ffmpeg -ss 00:00:00 -i cheering-%d.png -ss 00:00:00 -i Aida.m4a -filter:a "volume=0.8" -shortest cheering_raw.mp4
+ffmpeg -ss 00:00:00 -i cheering-%d.png cheering_raw.mp4
+
+# repair video
+HandBrakeCLI --crop 0:0:0:0 -i cheering_raw.mp4 -o cheering.mp4
+
+# clean up
+rm cheering-*.png
+rm cheering_raw.mp4
+
+# view :)
+#open cheering.mp4
