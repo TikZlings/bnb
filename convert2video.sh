c0fec7677007741b9556ea06d5af6c3eb37c4ab0
samcarter <sam.carter8@aol.com>
2019-01-30 10:08:08 +0100

    - adjusted confetti for longer video duration - adjusted sloth position for new height of banner - use music mixed with cheering

--- convert2video.sh
+++ convert2video.sh
@@ -5,7 +5,8 @@ rm cheering.mp4
 convert -density 160 cheering.pdf cheering.png
 
 # convert to video
-ffmpeg -ss 00:00:00 -i cheering-%d.png -ss 00:00:10 -i ToyStory.m4a -shortest cheering_raw.mp4
+ffmpeg -ss 00:00:00 -i cheering-%d.png -ss 00:00:00 -i ToyStory_mix.mp3 -shortest cheering_raw.mp4
+#ffmpeg -ss 00:00:00 -i cheering-%d.png -ss 00:00:10 -i ToyStory.m4a -shortest cheering_raw.mp4
 #ffmpeg -ss 00:00:00 -i cheering-%d.png cheering_raw.mp4
 
 # repair video
